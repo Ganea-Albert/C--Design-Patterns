12f96af2239b4cb8012e28115e4fd7238c1d52c1
Ganea-Albert <ganeaalbert704@gmail.com>
2022-02-08 17:28:50 +0200

    Second batch of structural codes

---
+++ Design-Patterns-CSharp/Design Patterns Project/Assets/Scripts/Command.Structure/Command.Structure/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Command
+    {
+        protected Receiver receiver;
+
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    public class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver) : base(receiver)
+        { }
+
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+
+    public class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+
+    public class Invoker
+    {
+        Command command;
+
+            public void SetCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            command.Execute();
+        }
+    }
+}
---
+++ Design-Patterns-CSharp/Design Patterns Project/Assets/Scripts/Observer.Structure/Observer.Structure/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            s.SubjectState = "ABC";
+            s.Notify();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Subject
+    {
+        private List<Observer> observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    public class ConcreteSubject : Subject
+    {
+        private string subjectState;
+
+        public string SubjectState
+        {
+            get { return subjectState; }
+            set { subjectState = value; }
+        }
+    }
+
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    public class ConcreteObserver : Observer
+    {
+        private string name;
+        private string observerState;
+        private ConcreteSubject subject;
+
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            this.subject = subject;
+            this.name = name;
+        }
+
+        public override void Update()
+        {
+            observerState = subject.SubjectState;
+            Console.WriteLine("Observer {0}'s new state is {1}", name, observerState);
+        }
+
+        public ConcreteSubject Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+    }
+}
